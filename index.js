03e16dd565678821e435a4b4a28fc4ae45f607e2
sumy7 <sumyggsun@gmail.com>
2018-05-23 13:53:55 +0800

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var emojies_defs = require('markdown-it-emoji/lib/data/full.json');
+var emojies_shortcuts = require('markdown-it-emoji/lib/data/shortcuts');
+var emoji_html = require('markdown-it-emoji/lib/render');
+var emoji_replace = require('markdown-it-emoji/lib/replace');
+var normalize_opts = require('markdown-it-emoji/lib/normalize_opts');
+
+var twemoji = require('twemoji');
+
+module.exports = function emoji_plugin(md, options) {
+    var defaults = {
+        defs: emojies_defs,
+        shortcuts: emojies_shortcuts,
+        enabled: []
+    };
+
+    var opts = normalize_opts(md.utils.assign({}, defaults, options || {}));
+
+    md.renderer.rules.emoji = function (token, idx) {
+        return twemoji.parse(token[idx].content);
+    };
+    md.core.ruler.push('emoji', emoji_replace(md, opts.defs, opts.shortcuts, opts.scanRE, opts.replaceRE));
+};
